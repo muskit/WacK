8e5f5d8daa74c341f1b0ed0a1e2f2da0ffcacf29
msk <15199219+muskit@users.noreply.github.com>
2023-10-09 18:03:30 -0700

    update SwipeArrow.cs

--- Scripts/Things/TunnelObjects/SwipeArrow.cs
+++ Scripts/Things/TunnelObjects/SwipeArrow.cs
@@ -25,11 +25,22 @@ namespace WacK.Things.TunnelObjects
         public void SetPosSize(int pos, int size)
 		{
 			var p = Position;
-			p.X = Constants.BASE_2D_RESOLUTION / 60 * pos + Constants.BASE_2D_RESOLUTION / 120;
-			Position = p;
-
 			var s = Size;
-			s.Y = Constants.BASE_2D_RESOLUTION / 60 * size - Constants.BASE_2D_RESOLUTION / 60;
+			
+			if (size <= 59)	
+			{
+				p.X = Constants.BASE_2D_RESOLUTION / 60 * pos + Constants.BASE_2D_RESOLUTION / 120;
+				s.Y = Constants.BASE_2D_RESOLUTION / 60 * size - Constants.BASE_2D_RESOLUTION / 60;
+			}
+			else
+			{
+				p.X = Constants.BASE_2D_RESOLUTION / 60 * pos;
+				s.Y = Constants.BASE_2D_RESOLUTION;
+
+			}
+
+			
+			Position = p;
 			Size = s;
 			shader.SetShaderParameter("TileMult", s.Y / 64);
 		}
